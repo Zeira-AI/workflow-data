a3c2416729481045f54af90b43afbfeb0d5c3921
Quy Dao Xuan <quydx@MacBook-Pro.local>
2026-01-20 20:22:45 +0700

    feat: add ferrero chocolate

--- app/constants.ts
+++ app/constants.ts
@@ -1,3 +1,3 @@
-export const VALID_CLIENTS = ['dsm-f', 'ferrero', 'kerry'];
+export const VALID_CLIENTS = ['dsm-f', 'ferrero', 'ferrero-chocolate', 'kerry'];
 
 export type ClientId = typeof VALID_CLIENTS[number];
