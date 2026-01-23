c9405bfc4a9027295a42e526c4bb9ca1e6b57c7b
Quy Dao Xuan <quydx@MacBook-Pro.local>
2026-01-23 11:01:55 +0700

    feat: kerry requirements

--- app/constants.ts
+++ app/constants.ts
@@ -1,3 +1,3 @@
-export const VALID_CLIENTS = ['dsm-f', 'ferrero', 'ferrero-chocolate', 'kerry'];
+export const VALID_CLIENTS = ['dsm-f', 'ferrero', 'ferrero-chocolate', 'kerry', 'kerry-requirements'];
 
 export type ClientId = typeof VALID_CLIENTS[number];
