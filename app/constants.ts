01ad76722c7dd9f3e2f07e708aa8a07ab2bbd0ca
Quy Dao Xuan <quydx@MacBook-Pro.local>
2026-02-02 14:50:12 +0700

    feat: add kalbe

--- app/constants.ts
+++ app/constants.ts
@@ -1,30 +1,34 @@
 export const CLIENTS = [
-    {
-        name: 'DSM-F',
-        value: 'dsm-f',
-    },
-    {
-        name: 'Ferrero',
-        value: 'ferrero',
-    },
-    {
-        name: 'Ferrero Chocolate',
-        value: 'ferrero-chocolate',
-    },
-    {
-        name: 'Kerry',
-        value: 'kerry',
-    },
-    {
-        name: 'Kerry Requirements',
-        value: 'kerry-requirements',
-    },
-    {
-        name: 'Croda',
-        value: 'croda',
-    },
+  {
+    name: "DSM-F",
+    value: "dsm-f",
+  },
+  {
+    name: "Ferrero",
+    value: "ferrero",
+  },
+  {
+    name: "Ferrero Chocolate",
+    value: "ferrero-chocolate",
+  },
+  {
+    name: "Kerry",
+    value: "kerry",
+  },
+  {
+    name: "Kerry Requirements",
+    value: "kerry-requirements",
+  },
+  {
+    name: "Croda",
+    value: "croda",
+  },
+  {
+    name: "Kalbe",
+    value: "kalbe",
+  },
 ] as const;
 
-export const VALID_CLIENTS = CLIENTS.map(c => c.value) as string[];
+export const VALID_CLIENTS = CLIENTS.map((c) => c.value) as string[];
 
-export type ClientId = typeof CLIENTS[number]['value'];
+export type ClientId = (typeof CLIENTS)[number]["value"];
